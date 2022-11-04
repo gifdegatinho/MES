4fcceaf56605d4ae3f385678ceae2c9099e5a9c3
maria_almeida <mariaedealmeida7@gmail.com>
2022-11-04 15:25:29 -0300

    Added material class

---
+++ MES.Model/Material.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MES.Model
+{
+    internal class Material
+    {
+        [Key, MaxLength(50)]
+        public string MaterialId { get; set; }
+        [Required, MaxLength(100)]
+        public string Description { get; set; }
+    }
+}
